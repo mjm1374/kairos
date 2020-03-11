d06d38c173e44fa70806a83913df9762b3e37a1c
Mike McAllister <mike@logikbox.com>
2020-03-10 21:15:37 -0400

    Update wp-config.php

--- dev/wp-config.php
+++ dev/wp-config.php
@@ -46,7 +46,15 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-//  salt here
+//  
+define('AUTH_KEY',         '');
+define('SECURE_AUTH_KEY',  '');
+define('LOGGED_IN_KEY',    '');
+define('NONCE_KEY',        '');
+define('AUTH_SALT',        '');
+define('SECURE_AUTH_SALT', '');
+define('LOGGED_IN_SALT',   '');
+define('NONCE_SALT',       '');
 
 /**#@-*/
 
